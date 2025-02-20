1d685c040dacee2185d2fa07501f257ace699d2c
rua-iri <rory.mcguigan@protonmail.com>
2025-02-20 12:03:31 +0300

    add: deployment script for each region

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+regions=(
+"us-west-2"
+"us-east-1"
+"eu-west-1"
+"ap-southeast-1"
+"ap-northeast-1"
+)
+
+
+for region in "${regions[@]}"; do
+
+    echo "Deploying to $region..."
+
+done;
