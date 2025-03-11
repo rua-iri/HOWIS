f11521d04571a2785ad84b3bf99169166e53af90
rua-iri <rory.mcguigan@protonmail.com>
2025-03-11 10:05:40 +0300

    update: implement siteURL class

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -1,6 +1,7 @@
 import json
+import traceback
 import requests
-import validators
+from site_url import SiteURL
 
 import logging
 
@@ -8,14 +9,14 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def check_site_status(site: str) -> tuple:
+def check_site_status(site: SiteURL) -> tuple:
     count_success = 0
     count_fail = 0
 
     for i in range(3):
         logger.info(f"Ping: {i}")
         try:
-            requests.head(site)
+            requests.head(site.get_https())
             count_success += 1
         except requests.ConnectionError:
             count_fail += 1
@@ -27,6 +28,8 @@ def dispatch(event, context):
     logger.info(event)
     logger.info(event.get("queryStringParameters"))
 
+    # TODO: take event from SNS not API gateway
+
     query_string: dict = event.get("queryStringParameters")
 
     if not query_string:
@@ -37,6 +40,8 @@ def dispatch(event, context):
     if not site:
         raise Exception("No Website Provided")
 
+    site: SiteURL = SiteURL(site)
+    logger.info(f"Site: {site.get_https}")
     count_success, count_fail = check_site_status(site)
 
     body = {
@@ -50,10 +55,8 @@ def dispatch(event, context):
 def lambda_handler(event, context):
     try:
         return dispatch(event, context)
-    
-    except validators.ValidationError:
-        return {"statusCode": 500, "body": json.dumps({"message": "Invalid URL Format"})}
 
     except Exception as e:
-        logger.info(e)
-        return {"statusCode": 500, "body": json.dumps("health check failed")}
+        logger.error(e)
+        logger.error(traceback.format_exc())
+        return {"statusCode": 500, "body": json.dumps({"error": "An error occurred"})}
