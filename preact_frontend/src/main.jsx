fd22b5f0a43b864475f3acebf035e8d3233a36c8
rua-iri <rory.mcguigan@protonmail.com>
2025-04-02 09:18:05 +0300

    update: disable console.log in production

--- preact_frontend/src/main.jsx
+++ preact_frontend/src/main.jsx
@@ -2,4 +2,10 @@ import { render } from "preact";
 import "./index.css";
 import { App } from "./app.jsx";
 
+const isEnvProd = import.meta.env.PROD;
+
+if (isEnvProd) {
+  console.log = () => {};
+}
+
 render(<App />, document.getElementById("app"));
